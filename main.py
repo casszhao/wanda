17b4d0f2165cef1027539bf099b3bf72e208d48a
Gchrysostomou <george1bodom@gmail.com>
2023-10-04 14:32:00 +0100

    testing on cpu, wanda not working will try gpu

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import argparse
-import os 
+import os
 import numpy as np
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
@@ -13,16 +13,21 @@ print('transformers', version('transformers'))
 #print('accelerate', version('accelerate'))
 print('# of gpus: ', torch.cuda.device_count())
 
-def get_llm(model_name, cache_dir="llm_weights"):
+def get_llm(model_name, cache_dir="llm_weights", device: str = 'auto'):
+    assert device in ['auto', 'cuda', 'cpu', 'mps']
+    if device == 'cpu':
+        torch_dtype=torch.float32
+    else:
+        torch_dtype=torch.float16
     model = AutoModelForCausalLM.from_pretrained(
-        model_name, 
-        torch_dtype=torch.float16, 
-        cache_dir=cache_dir, 
-        low_cpu_mem_usage=True, 
-        device_map="auto"
+        model_name,
+        torch_dtype=torch_dtype,
+        cache_dir=cache_dir,
+        low_cpu_mem_usage=True,
+        device_map=device
     )
 
-    model.seqlen = model.config.max_position_embeddings 
+    model.seqlen = model.config.max_position_embeddings
     return model
 
 def main():
@@ -37,6 +42,8 @@ def main():
     parser.add_argument('--use_variant', action="store_true", help="whether to use the wanda variant described in the appendix")
     parser.add_argument('--save', type=str, default="results", help='Path to save results.')
     parser.add_argument('--save_model', type=str, default="saved_model", help='Path to save the pruned model.')
+    parser.add_argument('--device', type=str, default="cpu", help='device for experiments')
+
     args = parser.parse_args()
 
     # Setting seeds for reproducibility
@@ -51,12 +58,16 @@ def main():
 
     model_name = args.model.split("/")[-1]
     print(f"loading llm model {args.model}")
-    model = get_llm(args.model, args.cache_dir)
+
+    if torch.cuda.device_count() > 0:
+        assert args.device == 'cuda', ("You have a gpu why not use it??")
+        model = get_llm(args.model, args.cache_dir, device='auto')
+    else:
+        model = get_llm(args.model, args.cache_dir, device=args.device)
     model.eval()
     tokenizer = AutoTokenizer.from_pretrained(args.model, use_fast=False, cache_dir = args.cache_dir)
-    #tokenizer = AutoTokenizer.from_pretrained("decapoda-research/llama-7b-hf", use_fast=False, cache_dir = "llm_weights")
 
-    device = torch.device("cuda:0")
+    device = torch.device(args.device)
     if "30b" in args.model or "65b" in args.model: # for 30b and 65b we use device_map to load onto multiple A6000 GPUs, thus the processing here.
         device = model.hf_device_map["lm_head"]
     print("use device ", device)
@@ -99,7 +110,7 @@ def main():
 
     model_save_path=f"pruned_model/{model_name}/{args.prune_method}"
     os.makedirs(model_save_path)
-    
+
     model.save_pretrained(model_save_path)
     tokenizer.save_pretrained(model_save_path)
 

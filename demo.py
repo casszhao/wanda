73fe9c459b3adb2180cff4141c53931ef4e85c6a
casszhao <cass790412@gmail.com>
2023-10-12 22:48:52 +0100

    from hpc cass

--- demo.py
+++ demo.py
@@ -3,14 +3,10 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from datasets import load_dataset,load_from_disk
 from rouge import FilesRouge, Rouge
+
+
 files_rouge = FilesRouge()
 rouge = Rouge()
-
-
-dataset = load_from_disk("data/dolly/") # 
-#features = ['instruction', 'context', 'response', 'category']
-example = dataset['train'][1]
-print(f"==>> example: {example}")
 
 
 
@@ -32,41 +28,50 @@ def testing_output(model_name, input):
     output = model.generate(input_ids, num_return_sequences=1) # do_sample=True, top_p=0.95, top_k=60, temperature=1, max_length=max_len, 
     output_text = tokenizer.decode(output[0], skip_special_tokens=True)
     print(' ')
-    
-    print(' ')
-    print(' ')
+
     return output_text
 
-# print(' =========================== full iput =========================== ')
-# #full_input = example['context'] + ' ' + example['instruction']
-# full_input = example['instruction'] + ' ' + example['context'] 
-# print(f"{full_input}")
-
-# output_text = testing_output("tiiuae/falcon-7b-instruct", full_input)
-# print(f"==>> output_text: {output_text}")
-# print(rouge.get_scores(output_text, example['response'], avg=True))
 
 
+def demo_print(example):
+    print(' ')
+    print(' ')
+    print(' =========================== full iput =========================== ')
+    full_input = example['instruction'] + ' ' + example['context'] + ' '
+    print(f"{full_input}")
+    print(' ')
 
-print(' =========================== full iput =========================== ')
-full_input = example['instruction'] + ' ' + example['context'] + ' '
-print(f"{full_input}")
-
-output_text = testing_output("tiiuae/falcon-7b-instruct", full_input)
-print(f"==>> output_text: {output_text}")
-print(rouge.get_scores(output_text, example['response'], avg=True))
+    output_text = testing_output("tiiuae/falcon-7b-instruct", full_input)
+    print(f" FULL MODEL OUTPUT: {output_text}")
+    print(' ')
+    print(rouge.get_scores(output_text, example['response'], avg=True)['rouge-l']['f'])
 
 
-model_index_name = "falcon-7b-instruct"
+    model_index_name = "falcon-7b-instruct"
 
-output_text1 = testing_output(f'pruned_model/{model_index_name}/sparsegpt', full_input)
-print(f"==>> output_text1: {output_text1}")
-print(rouge.get_scores(output_text1, example['response'], avg=True))
+    output_text1 = testing_output(f'pruned_model/{model_index_name}/sparsegpt', full_input)
+    print(f"==>> sparsegpt: {output_text1}")
+    print(' ')
+    print(rouge.get_scores(output_text1, example['response'], avg=True)['rouge-l']['f'])
 
-output_text2 = testing_output(f'pruned_model/{model_index_name}/magnitude/', full_input)
-print(f"==>> output_text2: {output_text2}")
-print(rouge.get_scores(output_text2, example['response'], avg=True))
+    output_text2 = testing_output(f'pruned_model/{model_index_name}/magnitude/', full_input)
+    print(f"==>> magnitude: {output_text2}")
+    print(' ')
+    print(rouge.get_scores(output_text2, example['response'], avg=True)['rouge-l']['f'])
 
-output_text3 = testing_output(f'pruned_model/{model_index_name}/wanda/', full_input)
-print(f"==>> output_text3: {output_text3}")
-print(rouge.get_scores(output_text3, example['response'], avg=True))
+    output_text3 = testing_output(f'pruned_model/{model_index_name}/wanda/', full_input)
+    print(f"==>> wanda: {output_text3}")
+    print(' ')
+    print(rouge.get_scores(output_text3, example['response'], avg=True)['rouge-l']['f'])
+
+
+
+dataset = load_from_disk("data/dolly/") # features = ['instruction', 'context', 'response', 'category']
+example = dataset['train'][1]
+demo_print(example)
+
+example = dataset['train'][2]
+demo_print(example)
+
+example = dataset['train'][3]
+demo_print(example)
